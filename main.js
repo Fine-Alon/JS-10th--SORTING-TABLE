2cc08e4a44c0d04d8c2ba2df5a35c88187ec31ff
Alon Fine <mecheniy5462@gmail.com>
2023-04-01 04:16:57 +0300

    add filtering of table by 4 params

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import Student from "./student.js";
-let $table = document.getElementById('table')
 
 
 const studentsList = [
-    new Student('Michail', 'Babojko', new Date('1994,04,02'), 2010, 'godofprogramming'),
+    new Student('Michail', 'Babojko', new Date('1995,04,02'), 2010, 'godofprogramming'),
     new Student('Alexandr', 'Dudukalo', new Date('March 30, 1994'), 2011, 'curator'),
     new Student('Alon', 'Fine', new Date('June 20, 1997'), 2019, 'fullstack'),
     new Student('Judit', 'Fine', new Date('September 10, 2005'), 2023, 'rocketsince'),
@@ -12,12 +11,21 @@ const studentsList = [
     new Student('Rick', 'Briens', new Date('July 20, 1987'), 2010, 'androiddev'),
 ]
 
+// important global variables
+let $table = document.getElementById('table'),
+    copyStList = [...studentsList],
+    $filterForm = document.getElementById('filterForm')
+
+
+
+// rendering of new Student to table
 function renderTable(arrStudents) {
 
+    document.querySelectorAll('.table-li').forEach(element => {
+        element.remove()
+    });
+
     arrStudents.forEach(element => {
-
-        console.log(element);
-
         let $liForTable = document.createElement('li')
         let $tableName = document.createElement('span')
         let $tableAge = document.createElement('span')
@@ -44,20 +52,56 @@ function renderTable(arrStudents) {
 }
 renderTable(studentsList)
 
+// add new Student to (Copy of main Arr) then (Render table)
 document.getElementById('form').addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(Number(document.getElementById('st-stady').value.trim()));
-
-    let copyStList = [...studentsList]
 
     copyStList.push(new Student(
         document.getElementById('st-name').value.trim(),
         (document.getElementById('st-surname').value).trim(),
         new Date(document.getElementById('st-dob').value),
-        (document.getElementById('st-faculty').value).trim(),
         Number(document.getElementById('st-stady').value.trim()),
+        (document.getElementById('st-faculty').value).trim(),
     ))
-    studentsList = [...copyStList]
-
+    renderTable(copyStList)
 })
 // console.log(studentsList[studentsList.length - 1].age);
+
+// This func check if 4/4 parametrs(Filtr Inputs) are True
+// initially all of them are True => cose we have no specific filters
+function filterStudentTable(name, facult, startEduc, endEduc) {
+
+    return copyStList.filter(student => {
+
+        let nameMatch = student.fullname.toLowerCase()
+        let facultMatch = student._facult.toLowerCase()
+        let startMatch = student._study
+        let endMatch = student.finishEducation
+
+        nameMatch = name ? nameMatch.includes(name.toLowerCase()) : true
+        facultMatch = facult ? facultMatch.includes(facult.toLowerCase()) : true
+        startMatch = startEduc ? startMatch >= startEduc : true
+        endMatch = endEduc ? endMatch <= endEduc : true
+
+        return nameMatch && facultMatch && startMatch && endMatch
+    })
+}
+
+// here we check every symbol that was input to Filter-Form
+// and send them to 'filterStudentTable(..,..,..,..)'
+// which filters out unnecessary
+let filterInputs = $filterForm.querySelectorAll('input')
+filterInputs.forEach(input => {
+
+    input.addEventListener('input', () => {
+
+        const filterName = document.getElementById('filter-name').value
+        const filterFacult = document.getElementById('filter-faculty').value
+        const filterStart = document.getElementById('filter-start-ed').value
+        const filterFinish = document.getElementById('filter-finish-ed').value
+
+        renderTable(filterStudentTable(filterName, filterFacult, filterStart, filterFinish))
+
+    })
+});
+
--- student.js
+++ student.js
@@ -1,7 +1,7 @@
 export default
     class Student {
 
-    constructor(name = 'name', surname = 'surname', birth = 'birth', study = 'study', facult = 'facult') {
+    constructor(name = 'name', surname = 'surname', birth = null, study = 'study', facult = 'facult') {
         this._name = name
         this._surname = surname
         this._birth = birth
@@ -13,16 +13,19 @@ export default
         return this._name + ' ' + this._surname
     }
 
+    get finishEducation() {
+        return Number(this._study) + 4
+    }
+
     get age() {
         var today = new Date();
         var birthDate = new Date(this._birth);
         var age = today.getFullYear() - birthDate.getFullYear();
         var m = today.getMonth() - birthDate.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
-        {
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
             age--;
         }
-        return age;
+        return `${birthDate.getFullYear()}.${birthDate.getMonth()}.${birthDate.getDate()}(${age}y.o)`
     }
 
 }
